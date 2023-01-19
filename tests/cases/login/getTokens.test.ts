6e9ed1013dde5c47c822ae3f6fd86a4edf8d3f30
Juraj Chripko <juraj770@gmail.com>
2023-01-19 11:32:48 +0100

    added tests for `Login.getTokens`

---
+++ tests/cases/login/getTokens.test.ts
@@ -0,0 +1,89 @@
+import express from 'express'
+import passport from 'passport'
+import { expect } from 'chai'
+
+import jsonwebtoken from 'jsonwebtoken'
+import { getUser, seedUsers } from '../../helpers'
+import { IJwtPayload, initAuth, IRefreshJwtPayload, Login } from '../../../src'
+
+import { UserRepository } from '../../mocks/repositories/userRepository'
+import { PasswordResetTokenRepository } from '../../mocks/repositories/passwordResetTokenRepository'
+import { TokenRepository } from '../../mocks/repositories/tokenRepository'
+
+describe('Password reset: getToken method', () => {
+	const userRepo = new UserRepository()
+	const passwordResetTokenRepo = new PasswordResetTokenRepository()
+	const app = express()
+
+	before(async () => {
+		await seedUsers(userRepo)
+
+		// init authentication library
+		initAuth(passport, {
+			userRepository: userRepo,
+			refreshTokenRepository: TokenRepository.getInstance(),
+			passwordResetTokenRepository: passwordResetTokenRepo
+		})
+
+		app.use(express.urlencoded({ extended: true }))
+		app.use(express.json())
+	})
+
+	it('No Family ID', async () => {
+		const user = getUser()
+		const result = await Login.getTokens(user.email)
+
+		/* eslint-disable @typescript-eslint/no-unused-expressions */
+		expect(result).to.exist
+		expect(result.accessToken).to.exist
+		expect(result.refreshToken).to.exist
+
+		/* eslint-enable */
+
+		const decodedAccessTokenData = <IJwtPayload & { id: number; permission: string }>jsonwebtoken.decode(result.accessToken)
+		const decodedRefreshTokenData = <IRefreshJwtPayload>jsonwebtoken.decode(result.refreshToken)
+		expect(decodedAccessTokenData.fid).to.eq(decodedAccessTokenData.rid)
+		expect(decodedAccessTokenData.fid).to.eq(decodedRefreshTokenData.fid)
+		expect(decodedAccessTokenData.fid).to.eq(decodedRefreshTokenData.jti)
+	})
+
+	it('With Family ID', async () => {
+		const user = getUser()
+		const familyID = 123456
+		const result = await Login.getTokens(user.email, familyID)
+
+		/* eslint-disable @typescript-eslint/no-unused-expressions */
+		expect(result).to.exist
+		expect(result.accessToken).to.exist
+		expect(result.refreshToken).to.exist
+
+		/* eslint-enable */
+
+		const decodedAccessTokenData = <IJwtPayload & { id: number; permission: string }>jsonwebtoken.decode(result.accessToken)
+		const decodedRefreshTokenData = <IRefreshJwtPayload>jsonwebtoken.decode(result.refreshToken)
+		expect(decodedAccessTokenData.fid).to.eq(familyID)
+		expect(decodedRefreshTokenData.fid).to.eq(familyID)
+		expect(decodedRefreshTokenData.jti).to.eq(familyID)
+	})
+
+	it('With payload', async () => {
+		const user = getUser()
+		const result = await Login.getTokens(user.email, undefined, { id: 5, permission: 'ADMINISTRATOR' })
+
+		/* eslint-disable @typescript-eslint/no-unused-expressions */
+		expect(result).to.exist
+		expect(result.accessToken).to.exist
+		expect(result.refreshToken).to.exist
+
+		/* eslint-enable */
+
+		const decodedAccessTokenData = <IJwtPayload & { id: number; permission: string }>jsonwebtoken.decode(result.accessToken)
+		const decodedRefreshTokenData = <IRefreshJwtPayload>jsonwebtoken.decode(result.refreshToken)
+		expect(decodedAccessTokenData.id).to.eq(5)
+		expect(decodedAccessTokenData.permission).to.eq('ADMINISTRATOR')
+		const familyID = decodedAccessTokenData.fid
+		expect(decodedAccessTokenData.fid).to.eq(familyID)
+		expect(decodedRefreshTokenData.fid).to.eq(familyID)
+		expect(decodedRefreshTokenData.jti).to.eq(familyID)
+	})
+})
