d266cca5b02c4b04687d2634dfcabe9d065dfd71
Juraj Chripko <juraj770@gmail.com>
2023-04-21 16:44:03 +0200

    fixed export of PasswordReset.workflow

--- src/passwordReset/index.ts
+++ src/passwordReset/index.ts
@@ -2,5 +2,6 @@ import getToken from './getToken'
 import guard from './guard'
 import { endpoint, requestSchema, responseSchema } from './endpoint'
 import { strategy, strategyVerifyFunction, secretOrKeyProvider } from './strategy'
+import workflow from './workflow'
 
-export { getToken, endpoint, requestSchema, responseSchema, guard, strategy, strategyVerifyFunction, secretOrKeyProvider }
+export { getToken, endpoint, requestSchema, responseSchema, guard, strategy, strategyVerifyFunction, secretOrKeyProvider, workflow }
